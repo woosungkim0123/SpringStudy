370e71eb74410141a2201f187af44f88a41563fc
woosungkim0123 <sibu2005@naver.com>
2022-09-20 23:29:04 +0900

    configuration에서 자바코드로는 5번 호출이지만 실상은 3번 호출 왜?

--- core/src/main/java/basic/core/member/MemberServiceImpl.java
+++ core/src/main/java/basic/core/member/MemberServiceImpl.java
@@ -1,6 +1,7 @@
 package basic.core.member;
 
 public class MemberServiceImpl implements MemberService {
+
     private final MemberRepository memberRepository;
 
     public MemberServiceImpl(MemberRepository memberRepository) {
@@ -16,4 +17,11 @@ public class MemberServiceImpl implements MemberService {
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
     }
+    /**
+     * 테스트 용도
+     */
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
+
 }
---
+++ core/src/test/java/basic/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,33 @@
+package basic.core.singleton;
+
+import basic.core.AppConfig;
+import basic.core.member.MemberRepository;
+import basic.core.member.MemberServiceImpl;
+import basic.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberRepository = " + memberRepository);
+        System.out.println("memberService -> memberServiceRepository = " + memberRepository1);
+        System.out.println("orderService -> memberServiceRepository = " + memberRepository2);
+
+        Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+}
