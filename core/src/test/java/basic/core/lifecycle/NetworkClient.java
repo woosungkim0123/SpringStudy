812f14f7e82281bf226c8069c7cae542e262098e
Woosung_Kim <sibu2005@naver.com>
2022-09-25 11:48:02 +0900

    javax 사용

--- core/src/test/java/basic/core/lifecycle/NetworkClient.java
+++ core/src/test/java/basic/core/lifecycle/NetworkClient.java
@@ -1,9 +1,10 @@
 package basic.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient {
 
     private String url;
 
@@ -30,14 +31,14 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     }
 
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    @PostConstruct
+    public void init() {
         connect();
         call("초기화 연결 메세지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    @PreDestroy
+    public void close() {
         disconnect();
     }
 }
