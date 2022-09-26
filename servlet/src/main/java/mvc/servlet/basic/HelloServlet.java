c1d4be0e67946a63fc0fb33baff1e6499be6f335
Woosung_Kim <sibu2005@naver.com>
2022-09-27 08:18:36 +0900

    첫 서블릿

--- servlet/src/main/java/mvc/servlet/basic/HelloServlet.java
+++ servlet/src/main/java/mvc/servlet/basic/HelloServlet.java
@@ -13,5 +13,11 @@ public class HelloServlet extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("HelloServlet.service");
         System.out.println("request = " + request);
+        System.out.println("response = " + response);
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
     }
 }
