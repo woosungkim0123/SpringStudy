4cacad1f3b17994e3ee231aed3a9cf4285777a90
Woosung_Kim <sibu2005@naver.com>
2022-09-29 08:13:33 +0900

    서블릿 프로젝트 시작

---
+++ servlet/src/main/java/mvc/servlet/web/servlet/MemberFormServlet.java
@@ -0,0 +1,21 @@
+package mvc.servlet.web.servlet;
+
+import mvc.servlet.domain.member.MemberRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "memberFormServlet", urlPatterns = "/servlet/members/new-form")
+public class MemberFormServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
