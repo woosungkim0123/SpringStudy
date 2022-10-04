267547470472b5398c5c046df99375b64b52fdce
Woosung_Kim <sibu2005@naver.com>
2022-10-05 00:50:27 +0900

    드디어 node에서 param으로 사용하는 spring에서는 PathVariable(경로변수) 등장. 역시 있을줄 알았어. 경로변수를 사용할 때마다 드는 고민이 언제 경로변수를 사용해야하는가 인데 node project할 때는 게시판의 모드에 따라 write 와 modify로 나눴는데 이렇게 나누는 형식이 맞는지도 모르겠고 다른 값이 들어오면 바로 에러페이지를 띄웟는데 mode에 의도치 않는 값이 들어오면 어떤식으로 에러처리를 해야하는지도 모르겠다. 공부하면서 알아보자

---
+++ springmvc/src/main/java/hello/springmvc/basic/requestMapping/MappingController.java
@@ -0,0 +1,48 @@
+package hello.springmvc.basic.requestMapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MappingController {
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping(value = "/hello-basic-v1", method = RequestMethod.GET)
+    public String helloBasic() {
+        log.info("helloBasic");
+
+        return "ok";
+    }
+
+    @GetMapping(value = "/mapping-get-v2")
+    public String mappingGetV2() {
+        log.info("mapping-get-v2");
+        return "ok";
+    }
+
+    @GetMapping("/mapping/{userId}")
+    public String mappingPath(@PathVariable("userId") String data) {
+        log.info("mappingPath userId={}", data);
+        return "ok";
+    }
+
+    @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+    public String mappingPath(@PathVariable String userId, @PathVariable Long orderId) {
+        log.info("mappingPath userId={}, orderId={}", userId, orderId);
+        return "ok";
+    }
+
+    @PostMapping(value = "/mapping-consume", consumes = "application/json")
+    public String mappingConsumes() {
+        log.info("mappingConsumes");
+        return "ok";
+    }
+
+    @PostMapping(value = "/mapping-produce", produces = "text/html")
+    public String mappingProduces() {
+        log.info("mappingProduces");
+        return "ok";
+    }
+}
