9f8378d0f1aa5b9b804f6e19377db7ff86da5204
Woosung_Kim <sibu2005@naver.com>
2022-10-09 22:39:52 +0900

    서비스단 코드. 특정 작업들을 db 쿼리로 처리해야할지 서비스단 코드로 처리해야할지에 대한 고민이 있음. 예시로 date_format 같은 것

---
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MemberService {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    // 회원 가입
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+
+    }
+    // 회원 전체 조회
+
+
+}
