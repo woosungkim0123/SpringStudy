ea4681ca2ceb6be65d8630a10dba5753247dcd3b
Woosung_Kim <sibu2005@naver.com>
2022-10-10 19:43:27 +0900

    stream 사용한 자바 8문법 복습 필요

--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -27,4 +27,17 @@ public class OrderItem {
     private int orderPrice; // 주문 가격
     private int count; // 주문수량
 
+    //==비즈니스 로직==//
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //==조회 로직==//
+    /**
+     * 주문상품 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
+
 }
