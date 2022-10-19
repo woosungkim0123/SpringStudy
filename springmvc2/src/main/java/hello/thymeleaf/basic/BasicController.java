305c88064b2711f78be2c0e614d72fa59bfb75b7
Woosung_Kim <sibu2005@naver.com>
2022-10-19 22:50:07 +0900

    간단한 타임리프 기능 설명

--- springmvc2/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ springmvc2/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -2,11 +2,14 @@ package hello.thymeleaf.basic;
 
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -21,6 +24,7 @@ public class BasicController {
         model.addAttribute("data", "Hello <b>Spring</b>");
         return "basic/text-basic";
     }
+
     @GetMapping("/variable")
     public String variable(Model model) {
         User userA = new User("userA", 10);
@@ -37,6 +41,26 @@ public class BasicController {
         model.addAttribute("userMap", map);
         return "basic/variable";
     }
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello " + data;
+        }
+    }
+
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
     @Data
     static class User {
 
