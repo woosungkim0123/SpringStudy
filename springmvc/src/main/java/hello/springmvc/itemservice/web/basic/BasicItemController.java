27a39e00eb0d3ea4d2900a146bd78bc19865d4cb
Woosung_Kim <sibu2005@naver.com>
2022-10-23 23:50:59 +0900

    타임리프 템플릿에서 편리한 기능 제공

--- springmvc/src/main/java/hello/springmvc/itemservice/web/basic/BasicItemController.java
+++ springmvc/src/main/java/hello/springmvc/itemservice/web/basic/BasicItemController.java
@@ -36,7 +36,8 @@ public class BasicItemController {
     }
 
     @GetMapping("/add")
-    public String addForm() {
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
         return "basic/addForm";
     }
 
