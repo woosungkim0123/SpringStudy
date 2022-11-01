6d5e8337db1349d11b1f9b591713c26a862bf971
Woosung_Kim <sibu2005@naver.com>
2022-11-01 22:11:35 +0900

    확실히 코드가 많이 줄어듬. 메세지코드리졸버를 사용했다는데 그냥 특정코드가 들어오면 찾아서 리턴하는 메서드가 아닐까?(추측)

--- SPRING_03_MVC_2/03__Validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ SPRING_03_MVC_2/03__Validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -113,35 +113,67 @@ public class ValidationItemControllerV2 {
 //        return"redirect:/validation/v2/items/{itemId}";
 //        }
 
-    @PostMapping("/add")
-    public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
-        if (!StringUtils.hasText(item.getItemName())) {
-            bindingResult.addError(
-                    new FieldError("item", "itemName", item.getItemName(), false, new String[]{"required.item.itemName"}, null, null)
-            );
+//    @PostMapping("/add")
+//    public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+//        if (!StringUtils.hasText(item.getItemName())) {
+//            bindingResult.addError(
+//                    new FieldError("item", "itemName", item.getItemName(), false, new String[]{"required.item.itemName"}, null, null)
+//            );
+//
+//        }
+//        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+//            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, new String[]{"range.item.price"}, new Object[]{1000, 1000000}, null));
+//        }
+//        if (item.getQuantity() == null || item.getQuantity() > 10000) {
+//            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, new String[]{"max.item.quantity"}, new Object[]{9999}, null));
+//        }
+////특정 필드 예외가 아닌 전체 예외
+//        if (item.getPrice() != null && item.getQuantity() != null) { int resultPrice = item.getPrice() * item.getQuantity();
+//            if (resultPrice < 10000) {
+//                bindingResult.addError(new ObjectError("item", new String[]{"totalPriceMin"}, new Object[]{10000, resultPrice}, null));
+//            }
+//        }
+//        if(bindingResult.hasErrors()) {
+//            log.info("errors={}", bindingResult);
+//            return "validation/v2/addForm";
+//        }
+//        //성공 로직
+//        Item savedItem = itemRepository.save(item);
+//        redirectAttributes.addAttribute("itemId",savedItem.getId());
+//        redirectAttributes.addAttribute("status",true);
+//        return"redirect:/validation/v2/items/{itemId}";
+//    }
 
+    @PostMapping("/add")
+    public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        log.info("objectName={}", bindingResult.getObjectName());
+        log.info("target={}", bindingResult.getTarget());
+        if (!StringUtils.hasText(item.getItemName())) {
+            bindingResult.rejectValue("itemName", "required");
         }
         if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
-            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, new String[]{"range.item.price"}, new Object[]{1000, 1000000}, null));
+            bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
         }
         if (item.getQuantity() == null || item.getQuantity() > 10000) {
-            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, new String[]{"max.item.quantity"}, new Object[]{9999}, null));
+            bindingResult.rejectValue("quantity", "max", new Object[]{9999}, null);
         }
-//특정 필드 예외가 아닌 전체 예외
-        if (item.getPrice() != null && item.getQuantity() != null) { int resultPrice = item.getPrice() * item.getQuantity();
+        //특정 필드 예외가 아닌 전체 예외
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
             if (resultPrice < 10000) {
-                bindingResult.addError(new ObjectError("item", new String[]{"totalPriceMin"}, new Object[]{10000, resultPrice}, null));
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
             }
         }
-        if(bindingResult.hasErrors()) {
+        if (bindingResult.hasErrors()) {
             log.info("errors={}", bindingResult);
             return "validation/v2/addForm";
         }
+
         //성공 로직
         Item savedItem = itemRepository.save(item);
-        redirectAttributes.addAttribute("itemId",savedItem.getId());
-        redirectAttributes.addAttribute("status",true);
-        return"redirect:/validation/v2/items/{itemId}";
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
     }
 
 
