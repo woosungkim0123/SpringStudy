f89353781e92b6c5f0687aa15fe415a8b408e3c4
Woosung_Kim <sibu2005@naver.com>
2022-11-02 21:44:50 +0900

    역시 컨트롤러가 너무 많은 기능을 들고 있었음. 분리하는게 맞는데 굳이 spring에서 제공하는 validator를 써야하는지에 관한 의문이 생김

--- SPRING_03_MVC_2/03__Validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ SPRING_03_MVC_2/03__Validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -25,6 +25,7 @@ import java.util.Map;
 public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
 
     @GetMapping
     public String items(Model model) {
@@ -144,32 +145,48 @@ public class ValidationItemControllerV2 {
 //        return"redirect:/validation/v2/items/{itemId}";
 //    }
 
+//    @PostMapping("/add")
+//    public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+//        log.info("objectName={}", bindingResult.getObjectName());
+//        log.info("target={}", bindingResult.getTarget());
+//
+//        if (bindingResult.hasErrors()) {
+//            log.info("errors={}", bindingResult);
+//            return "validation/v2/addForm";
+//        }
+//
+//        if (!StringUtils.hasText(item.getItemName())) {
+//            bindingResult.rejectValue("itemName", "required");
+//        }
+//        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+//            bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
+//        }
+//        if (item.getQuantity() == null || item.getQuantity() > 10000) {
+//            bindingResult.rejectValue("quantity", "max", new Object[]{9999}, null);
+//        }
+//        //특정 필드 예외가 아닌 전체 예외
+//        if (item.getPrice() != null && item.getQuantity() != null) {
+//            int resultPrice = item.getPrice() * item.getQuantity();
+//            if (resultPrice < 10000) {
+//                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+//            }
+//        }
+//        if (bindingResult.hasErrors()) {
+//            log.info("errors={}", bindingResult);
+//            return "validation/v2/addForm";
+//        }
+//
+//    //성공 로직
+//    Item savedItem = itemRepository.save(item);
+//        redirectAttributes.addAttribute("itemId",savedItem.getId());
+//        redirectAttributes.addAttribute("status",true);
+//        return"redirect:/validation/v2/items/{itemId}";
+//}
+
     @PostMapping("/add")
-    public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
-        log.info("objectName={}", bindingResult.getObjectName());
-        log.info("target={}", bindingResult.getTarget());
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        itemValidator.validate(item, bindingResult);
 
-        if (bindingResult.hasErrors()) {
-            log.info("errors={}", bindingResult);
-            return "validation/v2/addForm";
-        }
-
-        if (!StringUtils.hasText(item.getItemName())) {
-            bindingResult.rejectValue("itemName", "required");
-        }
-        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
-            bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
-        }
-        if (item.getQuantity() == null || item.getQuantity() > 10000) {
-            bindingResult.rejectValue("quantity", "max", new Object[]{9999}, null);
-        }
-        //특정 필드 예외가 아닌 전체 예외
-        if (item.getPrice() != null && item.getQuantity() != null) {
-            int resultPrice = item.getPrice() * item.getQuantity();
-            if (resultPrice < 10000) {
-                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
-            }
-        }
         if (bindingResult.hasErrors()) {
             log.info("errors={}", bindingResult);
             return "validation/v2/addForm";
