cf5bc011af900ecde4934cb202f3f8baae4427f0
Woosung_Kim <sibu2005@naver.com>
2022-11-07 23:38:36 +0900

    웹에서는 지속적으로 요청했을때만 세션 유지해주는데 웹뷰로 만든 앱의 경우 db에 세션을 저장해서 자동로그인 되도록 했는데 앱이랑 웹이랑 차이가 있는데 이 부분은 어떻게 처리한거지.. 고민.. 배민 앱이 로그인이 풀려본 기억이없는데?

--- SPRING_03_MVC_2/04__Login/src/main/java/hello/login/web/HomeController.java
+++ SPRING_03_MVC_2/04__Login/src/main/java/hello/login/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -55,7 +56,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model) {
 
         // 세션 관리자에 저장된 회원 정보 조회
@@ -66,6 +67,18 @@ public class HomeController {
         }
 
         Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginSpringV3(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+
         if (loginMember == null) {
             return "home";
         }
