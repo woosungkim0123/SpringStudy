b63ed5afb6d18767f515dca01978bd03b8668a05
Woosung_Kim <sibu2005@naver.com>
2022-11-09 23:30:22 +0900

    argumentResolver 만들어봄. 어떤식으로 동작하는지 알겠는데 공통으로 사용하는 것들에 대해서 어디까지 정의해야하는 부분에 대한 고민이 생김

--- SPRING_03_MVC_2/04__Login/src/main/java/hello/login/web/HomeController.java
+++ SPRING_03_MVC_2/04__Login/src/main/java/hello/login/web/HomeController.java
@@ -2,6 +2,7 @@ package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.argumentresolver.Login;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,20 +23,20 @@ public class HomeController {
     private final MemberRepository memberRepository;
     private final SessionManager sessionManager;
 
-//    @GetMapping("/")
+    //    @GetMapping("/")
     public String home() {
 
         return "home";
     }
 
-//    @GetMapping("/")
-    public String homeLogin(@CookieValue(name="memberId", required = false) Long memberId, Model model) {
+    //    @GetMapping("/")
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
         if (memberId == null) {
             return "home";
         }
 
         Member loginMember = memberRepository.findById(memberId);
-        if(loginMember == null) {
+        if (loginMember == null) {
             return "home";
         }
 
@@ -43,7 +44,7 @@ public class HomeController {
         return "loginHome";
     }
 
-//    @GetMapping("/")
+    //    @GetMapping("/")
     public String homeLoginV2(HttpServletRequest request, Model model) {
 
         // 세션 관리자에 저장된 회원 정보 조회
@@ -56,7 +57,7 @@ public class HomeController {
         return "loginHome";
     }
 
-//    @GetMapping("/")
+    //    @GetMapping("/")
     public String homeLoginV3(HttpServletRequest request, Model model) {
 
         // 세션 관리자에 저장된 회원 정보 조회
@@ -75,7 +76,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+    //    @GetMapping("/")
     public String homeLoginSpringV3(
             @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
 
@@ -83,6 +84,17 @@ public class HomeController {
             return "home";
         }
 
+        model.addAttribute("member", loginMember);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginV3ArgumentResolver(@Login Member loginMember, Model model) {
+        //세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "home";
+        }
+        //세션이 유지되면 로그인으로 이동
         model.addAttribute("member", loginMember);
         return "loginHome";
     }
