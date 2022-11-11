418b56d2cb442c6ddce178e2b3181d7be8c1f179
Woosung_Kim <sibu2005@naver.com>
2022-11-12 00:55:37 +0900

    서블릿으로 API에러 처리

--- SPRING_03_MVC_2/05__Exception/src/main/java/hello/exception/WebServerCustomizer.java
+++ SPRING_03_MVC_2/05__Exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -6,9 +6,8 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
-
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
         ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
