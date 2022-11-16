d101e9160c632ccf94bc8dc352a6198c61056f35
Woosung_Kim <sibu2005@naver.com>
2022-11-16 23:35:05 +0900

    리눅스에 파일 업로드 하는건 한번 봐야할듯. 그리고 FileStore만들어 놓는건 진짜 좋은듯

---
+++ SPRING_03_MVC_2/07__FileUpload/src/main/java/hello/upload/controller/ItemController.java
@@ -0,0 +1,30 @@
+package hello.upload.controller;
+
+import hello.upload.domain.ItemRepository;
+import hello.upload.domain.UploadFile;
+import hello.upload.file.FileStore;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemRepository itemRepository;
+    private final FileStore fileStore;
+
+    @GetMapping("/items/new")
+    public String newItem(@ModelAttribute ItemForm form) {
+        return "item-form";
+    }
+
+}
