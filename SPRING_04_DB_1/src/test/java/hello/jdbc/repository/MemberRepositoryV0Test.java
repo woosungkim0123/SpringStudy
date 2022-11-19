757366dac66dfea32dd7ea8fc2ef1daccf7f5a55
Woosung_Kim <sibu2005@naver.com>
2022-11-19 19:11:56 +0900

    LOW LEVEL 단에서 실행되는 내용을 이해하기 위해 JDBC 한번 사용해보기 - SELECT

--- SPRING_04_DB_1/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ SPRING_04_DB_1/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,12 +1,15 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 repository = new MemberRepositoryV0();
@@ -15,5 +18,11 @@ class MemberRepositoryV0Test {
     void crud() throws SQLException {
         Member member = new Member("memberV0", 10000);
         repository.save(member);
+
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("member == findMember", member == findMember);
+        log.info("member equals findMember", member.equals(findMember));
+        Assertions.assertThat(findMember).isEqualTo(member);
     }
+
 }
