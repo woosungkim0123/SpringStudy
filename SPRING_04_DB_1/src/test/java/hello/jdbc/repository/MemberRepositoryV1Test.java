614b61c28b1fda41b4a2010cbdb58d0822d4dd39
Woosung_Kim <sibu2005@naver.com>
2022-11-20 13:32:03 +0900

    항상 새로운 커넥션을 맺으면 당연하지만 TCP통신하고 하면 성능이 느릴 수 밖에 없음

---
+++ SPRING_04_DB_1/src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -0,0 +1,47 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.sql.SQLException;
+import java.util.NoSuchElementException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+class MemberRepositoryV1Test {
+
+    MemberRepositoryV1 repository;
+
+    @BeforeEach
+    void beforeEach() throws Exception {
+        //기본 DriverManager - 항상 새로운 커넥션 획득
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        repository = new MemberRepositoryV1(dataSource);
+    }
+
+    @Test
+    void crud() throws SQLException {
+        Member member = new Member("memberV3", 10000);
+        repository.save(member);
+
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("member == findMember", member == findMember);
+        log.info("member equals findMember", member.equals(findMember));
+        assertThat(findMember).isEqualTo(member);
+
+        repository.update(member.getMemberId(), 20000);
+        Member updatedMember = repository.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
+
+        repository.delete(member.getMemberId());
+        assertThatThrownBy(
+                () -> repository.findById(member.getMemberId())).isInstanceOf(NoSuchElementException.class);
+    }
+
+}
