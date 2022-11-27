20f06740310fd9ca768b67b2647bfb11f217ef30
Woosung_Kim <sibu2005@naver.com>
2022-11-28 00:00:50 +0900

    간단한 체크예외 Test

--- SPRING_04_DB_1/src/test/java/hello/jdbc/exception/basic/CheckedTest.java
+++ SPRING_04_DB_1/src/test/java/hello/jdbc/exception/basic/CheckedTest.java
@@ -1,9 +1,25 @@
 package hello.jdbc.exception.basic;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
 public class CheckedTest {
+
+    @Test
+    void checked_catch() {
+        Service service = new Service();
+        service.callCatch();
+    }
+
+    @Test
+    void checked_throw() {
+        Service service = new Service();
+        assertThatThrownBy(() -> service.callThrow()).isInstanceOf(MyCheckedException.class);
+    }
+
 
     /**
      * Exception을 상속받은 예외는 체크 예외가 된다.
