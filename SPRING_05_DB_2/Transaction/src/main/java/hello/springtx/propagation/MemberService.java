14d4b04505aba80920dd84964783de2fb22e2661
Woosung_Kim <sibu2005@naver.com>
2022-12-19 15:31:14 +0900

    transaction example - two each transaction
    MemberService @Transactional:OFF
    MemberRepository @Transactional:ON
    LogRepository @Transactional:ON

--- SPRING_05_DB_2/Transaction/src/main/java/hello/springtx/propagation/MemberService.java
+++ SPRING_05_DB_2/Transaction/src/main/java/hello/springtx/propagation/MemberService.java
@@ -25,4 +25,22 @@ public class MemberService {
         log.info("== logRepository 호출 종료 ==");
     }
 
+    public void joinV2(String username) {
+        Member member = new Member(username);
+        Log logMessage = new Log(username);
+
+        log.info("== memberRepository 호출 시작 ==");
+        memberRepository.save(member);
+        log.info("== memberRepository 호출 종료 ==");
+
+        log.info("== logRepository 호출 시작 ==");
+        try {
+            logRepository.save(logMessage);
+        } catch (RuntimeException e) {
+            log.info("log 저장에 실패했습니다. logMessage={}", logMessage.getMessage());
+            log.info("정상 흐름 변환");
+        }
+        log.info("== logRepository 호출 종료 ==");
+    }
+
 }
