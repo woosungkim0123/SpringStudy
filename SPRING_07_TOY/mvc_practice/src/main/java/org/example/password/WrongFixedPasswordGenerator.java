4037612c9b3bc32d63281ec34eca04e06c3fa0be
Woosung_Kim <sibu2005@naver.com>
2022-12-22 23:05:44 +0900

    make structure more easier to test

---
+++ SPRING_07_TOY/mvc_practice/src/main/java/org/example/password/WrongFixedPasswordGenerator.java
@@ -0,0 +1,8 @@
+package org.example.password;
+
+public class WrongFixedPasswordGenerator implements PasswordGenerator {
+    @Override
+    public String generatePassword() {
+        return "ab"; // 2글자
+    }
+}
