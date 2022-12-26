31633c73f92a75f6ad002a7189ec60902e07ec6b
Woosung_Kim <sibu2005@naver.com>
2022-12-26 22:20:52 +0900

    Singleton used spring container - rewrite alone and study one more

--- SPRING_01_CORE/src/test/java/basic/rewrite/singleton/SingletonTestRewrite.java
+++ SPRING_01_CORE/src/test/java/basic/rewrite/singleton/SingletonTestRewrite.java
@@ -5,6 +5,8 @@ import basic.rewrite.member.MemberServiceRewrite;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,4 +25,29 @@ public class SingletonTestRewrite {
 
         assertThat(memberService1).isNotSameAs(memberService2);
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+
+        SingletonServiceRewrite singletonService1 = SingletonServiceRewrite.getInstance();
+        SingletonServiceRewrite singletonService2 = SingletonServiceRewrite.getInstance();
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext appConfig = new AnnotationConfigApplicationContext(AppConfigRewrite.class);
+
+        MemberServiceRewrite memberService1 = appConfig.getBean("memberServiceRewrite", MemberServiceRewrite.class);
+        MemberServiceRewrite memberService2 = appConfig.getBean("memberServiceRewrite", MemberServiceRewrite.class);
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1).isSameAs(memberService2);
+    }
+
 }
