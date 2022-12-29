9f6b44d7d24f326c4232c4e158f36c9a8e7b1a8c
woosungKim <lomesame@gmail.com>
2022-12-29 17:43:03 +0900

    thymeleaf basicObject and Variable - one more study and rewrite

--- SPRING_03_MVC_2/01__UseThymeleafRewrite/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ SPRING_03_MVC_2/01__UseThymeleafRewrite/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,14 +1,17 @@
 package hello.thymeleaf.basic;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/basic")
@@ -28,22 +31,37 @@ public class BasicController {
 
     @GetMapping("/variable")
     public String variable(Model model) {
-
-        User userA = new User("사람A", 20);
-        User userB = new User("사람B", 25);
-
+        User userA = new User("김우성", 10);
+        User userB = new User("userB", 20);
         List<User> list = new ArrayList<>();
         list.add(userA);
         list.add(userB);
 
-        HashMap<String, User> map = new HashMap<>();
-        map
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
 
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+        return "basic/variable";
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "세션입니다");
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello " + data;
+        }
     }
 
     @Data
     static class User {
-
         private String username;
         private int age;
 
