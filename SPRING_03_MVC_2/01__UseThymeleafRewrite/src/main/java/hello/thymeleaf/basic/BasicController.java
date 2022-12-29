f5fd3f88ff8c0c7a8f895e56937ae723234f47e1
Woosung_Kim <sibu2005@naver.com>
2022-12-29 22:41:48 +0900

    thymeleaf if, switch - study and rewrite one more

--- SPRING_03_MVC_2/01__UseThymeleafRewrite/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ SPRING_03_MVC_2/01__UseThymeleafRewrite/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -53,6 +53,20 @@ public class BasicController {
         return "basic/basic-objects";
     }
 
+    @GetMapping("/each")
+    public String each(Model model) {
+        addUsers(model);
+        return "basic/each";
+    }
+
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+        model.addAttribute("users", list);
+    }
+
     @Component("helloBean")
     static class HelloBean {
         public String hello(String data) {
