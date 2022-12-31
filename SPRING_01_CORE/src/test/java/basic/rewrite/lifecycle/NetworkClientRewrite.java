da89240541e17ca928fae2ecd72fd23b53ab0bca
Woosung_Kim <sibu2005@naver.com>
2022-12-31 22:52:12 +0900

    beanlifeCycle - study and rewrite one more

---
+++ SPRING_01_CORE/src/test/java/basic/rewrite/lifecycle/NetworkClientRewrite.java
@@ -0,0 +1,28 @@
+package basic.rewrite.lifecycle;
+
+public class NetworkClientRewrite {
+
+    private String url;
+
+    public NetworkClientRewrite() {
+        System.out.println("생성자 호출 url = " + url);
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call = " + url + " message = " + message);
+    }
+
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+}
