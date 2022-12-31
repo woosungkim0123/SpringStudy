97f7985198fc7f8535c87da41a8f7e65a0a5be2c
Woosung_Kim <sibu2005@naver.com>
2022-12-31 22:54:55 +0900

    beanlifeCycle InitializingBean, DisposableBean - study and rewrite one more

--- SPRING_01_CORE/src/test/java/basic/rewrite/lifecycle/NetworkClientRewrite.java
+++ SPRING_01_CORE/src/test/java/basic/rewrite/lifecycle/NetworkClientRewrite.java
@@ -1,13 +1,14 @@
 package basic.rewrite.lifecycle;
 
-public class NetworkClientRewrite {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClientRewrite implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClientRewrite() {
         System.out.println("생성자 호출 url = " + url);
-        connect();
-        call("초기화 연결 메세지");
     }
 
     public void setUrl(String url) {
@@ -24,5 +25,16 @@ public class NetworkClientRewrite {
 
     public void disconnect() {
         System.out.println("close: " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        disconnect();
     }
 }
