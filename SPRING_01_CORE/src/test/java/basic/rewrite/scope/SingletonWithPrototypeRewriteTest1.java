ba4856984799da4d4de359419dbbbcd1f65ca249
Woosung_Kim <sibu2005@naver.com>
2023-01-01 22:21:54 +0900

    use provider - study and rewrite one more

--- SPRING_01_CORE/src/test/java/basic/rewrite/scope/SingletonWithPrototypeRewriteTest1.java
+++ SPRING_01_CORE/src/test/java/basic/rewrite/scope/SingletonWithPrototypeRewriteTest1.java
@@ -1,12 +1,14 @@
 package basic.rewrite.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,19 +35,20 @@ public class SingletonWithPrototypeRewriteTest1 {
         assertThat(count1).isEqualTo(1);
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
 
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
+        private final Provider<PrototypeBean> prototypeBeanProvider;
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
+        public ClientBean( Provider<PrototypeBean>  prototypeBeanProvider) {
+            this.prototypeBeanProvider = prototypeBeanProvider;
         }
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
