98dbb588707984fbd9d0bfb1a5f0b76c83a3d1b3
Woosung_Kim <sibu2005@naver.com>
2023-01-02 23:21:49 +0900

    apply provider and apply proxy object - study and rewrite one more

--- SPRING_01_CORE/src/main/java/basic/rewrite/web/LogDemoControllerRewrite.java
+++ SPRING_01_CORE/src/main/java/basic/rewrite/web/LogDemoControllerRewrite.java
@@ -2,6 +2,7 @@ package basic.rewrite.web;
 
 import basic.rewrite.common.MyLoggerRewrite;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,8 +19,7 @@ public class LogDemoControllerRewrite {
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
-        String requestURL;
-        requestURL = request.getRequestURL().toString();
+        String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL);
         myLogger.log("controller test");
         logDemoService.logic("testId");
--- SPRING_01_CORE/src/main/java/basic/rewrite/web/LogDemoServiceRewrite.java
+++ SPRING_01_CORE/src/main/java/basic/rewrite/web/LogDemoServiceRewrite.java
@@ -2,12 +2,14 @@ package basic.rewrite.web;
 
 import basic.rewrite.common.MyLoggerRewrite;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoServiceRewrite {
     private final MyLoggerRewrite myLogger;
+
     public void logic(String id) {
         myLogger.log("service id = " + id);
     }
