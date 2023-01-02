0f898f3440ac95b5758baffbec183c1aae90ec24
Woosung_Kim <sibu2005@naver.com>
2023-01-02 22:48:13 +0900

    make scope request but error. why? spring do di but not found bean. because MyLoggerRewrite has request scope. so not made - study and rewrite one more

---
+++ SPRING_01_CORE/src/main/java/basic/rewrite/web/LogDemoControllerRewrite.java
@@ -0,0 +1,28 @@
+package basic.rewrite.web;
+
+import basic.rewrite.common.MyLoggerRewrite;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoControllerRewrite {
+
+    private final LogDemoServiceRewrite logDemoService;
+    private final MyLoggerRewrite myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody
+    public String logDemo(HttpServletRequest request) {
+        String requestURL;
+        requestURL = request.getRequestURL().toString();
+        myLogger.setRequestURL(requestURL);
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "OK";
+    }
+}
