13b1ecf9d4864a7fcc2a4d9ef103987a1b180c94
Woosung_Kim <sibu2005@naver.com>
2023-01-05 21:28:26 +0900

    request getParameter - study one more and rewrite

---
+++ SPRING_02_MVC_1/01__ServletRewrite/src/test/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,23 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> System.out.println(paramName + "=" + request.getParameter(paramName)));
+
+        System.out.println();
+        String username = request.getParameter("username");
+        System.out.println("request.getParameter(username) = " + username);
+
+        response.getWriter().write("ok");
+    }
+}
