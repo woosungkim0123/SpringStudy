1b87fc2811384c489930649ded6945da2a7c0221
Woosung_Kim <sibu2005@naver.com>
2023-01-13 00:21:23 +0900

    start write test. after make test,implement code what test is error or not exist.

---
+++ SPRING_07_TOY/01_MVC_PRACTICE/src/main/java/org/example/Calculator.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Calculator {
+    public static int calculate(int operand1, String operator, int operand2) {
+        return ArithmeticOperator.calculate(operand1, operator, operand2);
+    }
+}
