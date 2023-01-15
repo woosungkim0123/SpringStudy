08ad983c78054e22f1c9d72b868ea7dfad1d505a
Woosung_Kim <sibu2005@naver.com>
2023-01-15 23:36:05 +0900

    make RedirectAttribute because of PRG

--- SPRING_02_MVC_1/03__ItemService/src/main/java/hello/itemservice/domain/item/basic/BasicItemController.java
+++ SPRING_02_MVC_1/03__ItemService/src/main/java/hello/itemservice/domain/item/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -36,10 +37,13 @@ public class BasicItemController {
     public String addForm() {
         return "basic/addForm";
     }
+
     @PostMapping("/add")
-    public String addItem(Item item) {
-        itemRepository.save(item);
-        return "basic/item";
+    public String addItem(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
