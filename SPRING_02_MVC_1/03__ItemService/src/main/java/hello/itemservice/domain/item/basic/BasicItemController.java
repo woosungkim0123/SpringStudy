d05f02074e99dfdeb602efb91d09abb451931f3e
Woosung_Kim <sibu2005@naver.com>
2023-01-15 16:54:09 +0900

    make controller had item list page and test init method

---
+++ SPRING_02_MVC_1/03__ItemService/src/main/java/hello/itemservice/domain/item/basic/BasicItemController.java
@@ -0,0 +1,34 @@
+package hello.itemservice.domain.item.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute(items);
+        return "basic/items";
+    }
+
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("testA", 10000, 10));
+        itemRepository.save(new Item("testB", 15000, 20));
+    }
+}
