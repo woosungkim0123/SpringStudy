03e1d0a3096ead9efca6860b1fb64486120a6302
Woosung_Kim <sibu2005@naver.com>
2023-01-16 15:47:59 +0900

    divide editForm and addForm

--- SPRING_03_MVC_2/03__ValidationRewrite/src/main/java/hello/itemservice/web/validation/ItemUpdateForm.java
+++ SPRING_03_MVC_2/03__ValidationRewrite/src/main/java/hello/itemservice/web/validation/ItemUpdateForm.java
@@ -1,4 +1,4 @@
-package hello.itemservice.domain.item;
+package hello.itemservice.web.validation;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
@@ -8,19 +8,16 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
-public class Item {
-
+public class ItemUpdateForm {
+    @NotNull
     private Long id;
+
+    @NotBlank
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+
     private Integer quantity;
-
-    public Item() {
-    }
-
-    public Item(String itemName, Integer price, Integer quantity) {
-        this.itemName = itemName;
-        this.price = price;
-        this.quantity = quantity;
-    }
 }
