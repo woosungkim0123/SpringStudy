0204bbc57d46cfd2b35ba920c6522245ade581fd
Woosung_Kim <sibu2005@naver.com>
2023-01-16 10:07:23 +0900

    change another ObjectError constructor and FileError constructor

--- SPRING_03_MVC_2/03__ValidationRewrite/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ SPRING_03_MVC_2/03__ValidationRewrite/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -49,23 +49,22 @@ public class ValidationItemControllerV2 {
     public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
         if(!StringUtils.hasText(item.getItemName())) {
-            bindingResult.addError(new FieldError("item", "itemName", "상품 이름은 필수입니다."));
+            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, null, null, "상품 이름은 필수입니다."));
         }
         if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
-            bindingResult.addError(new FieldError("item", "price", "가격은 1,000 ~ 1,000,000 까지 허용합니다."));
+            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null, "가격은 1,000 ~ 1,000,000 까지 허용합니다."));
         }
         if(item.getQuantity() == null || item.getQuantity() >= 9999) {
-            bindingResult.addError(new FieldError("item", "quantity", "수량은 최대 9,999 까지 허용합니다."));
+            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, null, null, "수량은 최대 9,999 까지 허용합니다."));
         }
 
         if(item.getPrice() != null && item.getQuantity() != null) {
             int resultPrice = item.getPrice() * item.getQuantity();
             if(resultPrice < 10000) {
-                bindingResult.addError(new ObjectError("item", "최종가격은 10000원을 넘어야합니다. 현재값 = " + resultPrice));
+                bindingResult.addError(new ObjectError("item", null, null, "최종가격은 10000원을 넘어야합니다. 현재값 = " + resultPrice));
             }
         }
         if(bindingResult.hasErrors()) {
-            log.info("여기옴");
             log.info("errors={}", bindingResult);
             return "validation/v2/addForm";
         }
