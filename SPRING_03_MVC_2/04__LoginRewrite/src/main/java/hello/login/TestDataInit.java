425d8573432f5ab10ebaa16c76670d94ff44d6c9
Woosung_Kim <sibu2005@naver.com>
2023-01-17 10:51:29 +0900

    init add test id

--- SPRING_03_MVC_2/04__LoginRewrite/src/main/java/hello/login/TestDataInit.java
+++ SPRING_03_MVC_2/04__LoginRewrite/src/main/java/hello/login/TestDataInit.java
@@ -2,6 +2,8 @@ package hello.login;
 
 import hello.login.domain.item.Item;
 import hello.login.domain.item.ItemRepository;
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,14 +14,22 @@ import javax.annotation.PostConstruct;
 public class TestDataInit {
 
     private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
 
     /**
      * 테스트용 데이터 추가
      */
     @PostConstruct
     public void init() {
+        System.out.println("의존성 주입 끝나고 실행");
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
+
+        Member member = new Member();
+        member.setLoginId("test");
+        member.setPassword("test");
+        member.setName("테스터");
+        memberRepository.save(member);
     }
 
 }
