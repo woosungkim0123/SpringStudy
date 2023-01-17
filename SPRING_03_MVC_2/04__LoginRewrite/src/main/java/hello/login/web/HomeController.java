c831c60a92be11a4517348b54d6469d630ed6fd2
Woosung_Kim <sibu2005@naver.com>
2023-01-17 14:26:26 +0900

    remove memberRepository in HomeController

--- SPRING_03_MVC_2/04__LoginRewrite/src/main/java/hello/login/web/HomeController.java
+++ SPRING_03_MVC_2/04__LoginRewrite/src/main/java/hello/login/web/HomeController.java
@@ -1,13 +1,11 @@
 package hello.login.web;
 
 import hello.login.domain.member.Member;
-import hello.login.domain.member.MemberRepository;
 import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,8 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
-
-    private final MemberRepository memberRepository;
 
     private final SessionManager sessionManager;
 
