49f1bcb11fe8456b48cff6751f1a3fb02597e5b1
Woosung_Kim <sibu2005@naver.com>
2023-01-25 16:25:55 +0900

    string to integer convert implement and test

--- SPRING_03_MVC_2/06__TypeConverterRewrite/src/main/java/hello/typeconverter/converter/StringToIntegerConverter.java
+++ SPRING_03_MVC_2/06__TypeConverterRewrite/src/main/java/hello/typeconverter/converter/StringToIntegerConverter.java
@@ -4,10 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.convert.converter.Converter;
 
 @Slf4j
-public class StringToIntegerConverter implements Converter<Integer, String> {
+public class StringToIntegerConverter implements Converter<String, Integer> {
     @Override
-    public String convert(Integer source) {
+    public Integer convert(String source) {
         log.info("convert source = {}", source);
-        return String.valueOf(source);
+        return Integer.valueOf(source);
     }
 }
---
+++ SPRING_03_MVC_2/06__TypeConverterRewrite/src/test/java/hello/typeconverter/converter/StringToIntegerConverterTest.java
@@ -0,0 +1,17 @@
+package hello.typeconverter.converter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringToIntegerConverterTest {
+
+    @Test
+    void stringToInteger() {
+        StringToIntegerConverter converter = new StringToIntegerConverter();
+        Integer result = converter.convert("10");
+        assertThat(result).isEqualTo(10);
+    }
+}
