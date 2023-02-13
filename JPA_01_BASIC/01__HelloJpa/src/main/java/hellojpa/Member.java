6e9e338d64379b78538bca07d25636258ad7f29f
Woosung_Kim <sibu2005@naver.com>
2023-02-13 20:06:39 +0900

    db key - identity strategy

--- JPA_01_BASIC/01__HelloJpa/src/main/java/hellojpa/Member.java
+++ JPA_01_BASIC/01__HelloJpa/src/main/java/hellojpa/Member.java
@@ -6,26 +6,11 @@ import java.util.Date;
 @Entity
 public class Member {
 
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name="name")
+    @Column(name="name", nullable = false)
     private String username;
-
-    private Integer age;
-
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    @Lob
-    private String description;
-
 
     public Member() {
 
