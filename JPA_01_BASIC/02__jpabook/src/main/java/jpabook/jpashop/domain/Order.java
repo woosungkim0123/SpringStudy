026110b58ef10628711f316693dcda3b7a470baa
Woosung_Kim <sibu2005@naver.com>
2023-02-17 00:02:27 +0900

    jpa - many to one

--- JPA_01_BASIC/02__jpabook/src/main/java/jpabook/jpashop/domain/Order.java
+++ JPA_01_BASIC/02__jpabook/src/main/java/jpabook/jpashop/domain/Order.java
@@ -10,11 +10,49 @@ public class Order {
     @Column(name = "ORDER_ID")
     private Long id;
 
-    @Column(name = "MEMBER_ID")
-    private Long memberId;
+    // @Column(name = "MEMBER_ID")
+    // private Long memberId;
+
+    // ORDER 입장에선 ManyToOne
+    // MEMBER입장 에선 하나의 회원이 여러개 주문할 수 있어서 1 : N인데 ORDER 입장에선 나를 주문한 사람은 1명 N : 1
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
 
     private LocalDateTime orderDate;
+
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
 }
