985ddd9550760dcc32a27caeaa242f6c6304ad73
Woosung_Kim <sibu2005@naver.com>
2023-02-23 23:40:38 +0900

    모든 연관관계에 지연 로딩으로 변경

--- JPA_01_BASIC/02__jpabook/src/main/java/jpabook/jpashop/domain/Order.java
+++ JPA_01_BASIC/02__jpabook/src/main/java/jpabook/jpashop/domain/Order.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Table(name="ORDERS")
 public class Order extends BaseEntity {
@@ -12,11 +14,11 @@ public class Order extends BaseEntity {
     @Column(name = "ORDER_ID")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
-    @OneToOne
+    @OneToOne(fetch = LAZY)
     @JoinColumn(name = "DELIVERY_ID")
     private Delivery delivery;
 
