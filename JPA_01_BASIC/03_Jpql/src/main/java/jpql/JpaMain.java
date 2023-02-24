8ed5077fe8354792a13f9eedf6ef87984121adac
Woosung_Kim <sibu2005@naver.com>
2023-02-25 00:04:08 +0900

    LAZY 지연로딩, 페이징

--- JPA_01_BASIC/03_Jpql/src/main/java/jpql/JpaMain.java
+++ JPA_01_BASIC/03_Jpql/src/main/java/jpql/JpaMain.java
@@ -2,6 +2,7 @@ package jpql;
 
 
 import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -12,15 +13,26 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 예시코드 (pseudocode)
-            Member member = new Member();
-            member.setUsername("우성");
-            member.setAge(10);
-            em.persist(member);
+            for(int i= 0 ; i < 100; i++) {
+                Member member = new Member();
+                member.setUsername("우성" + i);
+                member.setAge(i);
+                em.persist(member);
+            }
 
-            TypedQuery<Member> query = em.createQuery("select m from Member m", Member.class);
-            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
-            Query query1 = em.createQuery("select m.username, m.age from Member m");
+
+            em.flush();
+            em.clear();
+
+            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(0)
+                    .setMaxResults(10)
+                    .getResultList();
+
+            System.out.println("result.size() = " + result.size());
+            for (Member member1 : result) {
+                System.out.println("member1 = " + member1);
+            }
 
             tx.commit();
         } catch(Exception e) {
--- JPA_01_BASIC/03_Jpql/src/main/java/jpql/Member.java
+++ JPA_01_BASIC/03_Jpql/src/main/java/jpql/Member.java
@@ -10,7 +10,7 @@ public class Member {
     private String username;
     private int age;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "TEAM_ID")
     private Team team;
 
@@ -36,5 +36,14 @@ public class Member {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
