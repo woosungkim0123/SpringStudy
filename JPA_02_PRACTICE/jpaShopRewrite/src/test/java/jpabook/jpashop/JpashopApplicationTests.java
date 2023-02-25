51c9913c9cb906226ca58657184a4d0a388033fc
Woosung_Kim <sibu2005@naver.com>
2023-02-25 18:49:13 +0900

    class 못찾아서 명명

--- JPA_02_PRACTICE/jpaShopRewrite/src/test/java/jpabook/jpashop/JpashopApplicationTests.java
+++ JPA_02_PRACTICE/jpaShopRewrite/src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -3,7 +3,7 @@ package jpabook.jpashop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = JpashopApplication.class)
 class JpashopApplicationTests {
 
 	@Test
