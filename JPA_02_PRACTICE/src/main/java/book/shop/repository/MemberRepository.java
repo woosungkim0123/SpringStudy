414aa69b017b41a45939d12cf546721aeae27e75
Woosung_Kim <sibu2005@naver.com>
2023-02-26 22:23:05 +0900

    멤버 엔티티와 Repository 생성

---
+++ JPA_02_PRACTICE/src/main/java/book/shop/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package book.shop.repository;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+}
