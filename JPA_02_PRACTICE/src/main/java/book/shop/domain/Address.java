d4f7f2eea2b2057b4dbaa670f66e8570463b32c7
Woosung_Kim <sibu2005@naver.com>
2023-03-01 00:54:52 +0900

    setter 제거

--- JPA_02_PRACTICE/src/main/java/book/shop/domain/Address.java
+++ JPA_02_PRACTICE/src/main/java/book/shop/domain/Address.java
@@ -1,7 +1,6 @@
 package book.shop.domain;
 
 import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.Embeddable;
 
@@ -11,4 +10,13 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() {
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
