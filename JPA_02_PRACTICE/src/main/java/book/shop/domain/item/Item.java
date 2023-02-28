21928c69be5ff26ec51c0c25c84adc5a01560c3f
Woosung_Kim <sibu2005@naver.com>
2023-03-01 00:04:23 +0900

    자기 자신에 대해서 1:N, N:1

--- JPA_02_PRACTICE/src/main/java/book/shop/domain/item/Item.java
+++ JPA_02_PRACTICE/src/main/java/book/shop/domain/item/Item.java
@@ -21,6 +21,6 @@ public abstract class Item {
     private int price;
     private int stockQuantity;
 
-    @ManyToMany
+    @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 }
