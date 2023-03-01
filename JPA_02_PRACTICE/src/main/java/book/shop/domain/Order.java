2df9f134fc6f7e8865ce5e9aa859318adc5b0856
Woosung_Kim <sibu2005@naver.com>
2023-03-01 20:26:46 +0900

    cascade 추가 연관관계 메서드 추가

--- JPA_02_PRACTICE/src/main/java/book/shop/domain/Order.java
+++ JPA_02_PRACTICE/src/main/java/book/shop/domain/Order.java
@@ -21,14 +21,21 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private Delivery delivery;
 
     private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
 }
