92f82cb597fb632582bb49a203be9273de230caf
Woosung_Kim <sibu2005@naver.com>
2023-03-02 00:40:37 +0900

    이름 중복 에러 테스트

--- JPA_02_PRACTICE/src/test/java/book/shop/service/MemberServiceTest.java
+++ JPA_02_PRACTICE/src/test/java/book/shop/service/MemberServiceTest.java
@@ -2,11 +2,13 @@ package book.shop.service;
 
 import book.shop.domain.Member;
 import book.shop.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import javax.persistence.EntityManager;
 
@@ -38,6 +40,19 @@ class MemberServiceTest {
 
     @Test
     public void duplicateCheck() {
+        // given
+        Member member1 = new Member();
+        member1.setName("우성");
+        Member member2 = new Member();
+        member2.setName("우성");
 
+        // when
+        System.out.println(" ================1 ");
+        memberService.join(member1);
+
+        // then
+        System.out.println(" ================ 2");
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        System.out.println(" ================ 3");
     }
 }
