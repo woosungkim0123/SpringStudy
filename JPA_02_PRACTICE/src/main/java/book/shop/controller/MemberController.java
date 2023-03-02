727fe169d0264356e2e7208bbec8704102ede959
Woosung_Kim <sibu2005@naver.com>
2023-03-02 21:21:40 +0900

    에러 validation

--- JPA_02_PRACTICE/src/main/java/book/shop/controller/MemberController.java
+++ JPA_02_PRACTICE/src/main/java/book/shop/controller/MemberController.java
@@ -1,10 +1,16 @@
 package book.shop.controller;
 
+import book.shop.domain.Address;
+import book.shop.domain.Member;
 import book.shop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,6 +22,24 @@ public class MemberController {
     public String createForm(Model model) {
         model.addAttribute("memberForm", new MemberForm());
         return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        if(result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 
 }
