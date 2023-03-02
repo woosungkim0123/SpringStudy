9c5d52b17a676d2da3649868e9cdfa68fcb94027
Woosung_Kim <sibu2005@naver.com>
2023-03-02 10:31:46 +0900

    도메인에 추가 삭제 메서드 넣음. 응집력 up

--- JPA_02_PRACTICE/src/main/java/book/shop/domain/item/Item.java
+++ JPA_02_PRACTICE/src/main/java/book/shop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package book.shop.domain.item;
 
 import book.shop.domain.Category;
+import book.shop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,4 +24,16 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
