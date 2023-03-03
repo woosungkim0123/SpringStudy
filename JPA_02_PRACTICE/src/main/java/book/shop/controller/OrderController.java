b62bea6e4b70a379f31c5e83c5c684a2bf73d64c
Woosung_Kim <sibu2005@naver.com>
2023-03-03 15:54:21 +0900

    주문 리스트, 검색 동적 쿼리 jpql 사용

--- JPA_02_PRACTICE/src/main/java/book/shop/controller/OrderController.java
+++ JPA_02_PRACTICE/src/main/java/book/shop/controller/OrderController.java
@@ -1,6 +1,8 @@
 package book.shop.controller;
 
 import book.shop.domain.Member;
+import book.shop.domain.Order;
+import book.shop.domain.OrderSearch;
 import book.shop.domain.item.Item;
 import book.shop.service.ItemService;
 import book.shop.service.MemberService;
@@ -8,9 +10,7 @@ import book.shop.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,6 +33,19 @@ public class OrderController {
     @PostMapping(value = "/order")
     public String order(@RequestParam("memberId") Long memberId, @RequestParam("itemId") Long itemId, @RequestParam("count") int count) {
         orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+
+    @GetMapping(value = "/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) {
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+        return "order/orderList";
+    }
+
+    @PostMapping(value = "/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId) {
+        orderService.cancelOrder(orderId);
         return "redirect:/orders";
     }
 }
