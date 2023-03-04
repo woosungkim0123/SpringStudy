b8a48d2767f99bd74c179283815746cad0eb5437
Woosung_Kim <sibu2005@naver.com>
2023-03-05 00:44:14 +0900

    dto 사용 및 result로 한번 감쌈

--- JPA_02_PRACTICE/src/main/java/book/shop/api/MemberApiController.java
+++ JPA_02_PRACTICE/src/main/java/book/shop/api/MemberApiController.java
@@ -2,6 +2,7 @@ package book.shop.api;
 
 import book.shop.domain.Member;
 import book.shop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +23,23 @@ public class MemberApiController {
     @GetMapping("/api/v1/members")
     public List<Member> membersV1() {
         return memberService.findMembers();
+    }
+    @GetMapping("/api/v2/members")
+    public Result membersV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream().map(m -> new MemberDto(m.getName())).collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
     }
 
     @PostMapping("/api/v1/members")
