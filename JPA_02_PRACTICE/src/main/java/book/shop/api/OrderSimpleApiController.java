bd042f59ec1e97c95f13fba5f50a8508bef9571b
Woosung_Kim <sibu2005@naver.com>
2023-03-05 23:26:05 +0900

    엔티티를 반환함으로써 jackson 라이브러리가 무한루프에 빠짐

---
+++ JPA_02_PRACTICE/src/main/java/book/shop/api/OrderSimpleApiController.java
@@ -0,0 +1,27 @@
+package book.shop.api;
+
+import book.shop.domain.Order;
+import book.shop.domain.OrderSearch;
+import book.shop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+/**
+ * ToOne(ManyToONE, OneToOne)
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        return orderRepository.findAllByString(new OrderSearch());
+    }
+
+}
