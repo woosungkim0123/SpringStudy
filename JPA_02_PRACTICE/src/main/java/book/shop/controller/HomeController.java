2377eb1a0a19449a215f6abf7b04dcfbcc253df5
Woosung_Kim <sibu2005@naver.com>
2023-03-05 22:59:49 +0900

    api 성능 향상을 위한 기본 데이터 셋팅

--- JPA_02_PRACTICE/src/main/java/book/shop/controller/HomeController.java
+++ JPA_02_PRACTICE/src/main/java/book/shop/controller/HomeController.java
@@ -1,9 +1,14 @@
 package book.shop.controller;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @Controller
 @Slf4j
@@ -13,4 +18,6 @@ public class HomeController {
     public String home() {
         return "home";
     }
+
+
 }
