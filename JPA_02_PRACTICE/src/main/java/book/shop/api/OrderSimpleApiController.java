fa7fcfb6d579c2bc80e6397ca159c717da2b7190
Woosung_Kim <sibu2005@naver.com>
2023-03-06 18:13:01 +0900

    엔티티 방식 v1 -> v2 dto 사용 lazy 초기화로 인해서 쿼리가 어마하게 나가서 성능 저하(n + 1 문제)

--- JPA_02_PRACTICE/src/main/java/book/shop/api/OrderSimpleApiController.java
+++ JPA_02_PRACTICE/src/main/java/book/shop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package book.shop.api;
 
+import book.shop.domain.Address;
 import book.shop.domain.Order;
 import book.shop.domain.OrderSearch;
+import book.shop.domain.OrderStatus;
 import book.shop.repository.OrderRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 /**
@@ -23,5 +28,32 @@ public class OrderSimpleApiController {
     public List<Order> ordersV1() {
         return orderRepository.findAllByString(new OrderSearch());
     }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        return orderRepository
+                .findAllByString(new OrderSearch())
+                .stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName();
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getStatus();
+            this.address = order.getDelivery().getAddress();
+        }
+    }
+
 
 }
