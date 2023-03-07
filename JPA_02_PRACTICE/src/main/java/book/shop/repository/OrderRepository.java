c94b4fcfdc75335ec565d2e0e5853f69741d1442
Woosung_Kim <sibu2005@naver.com>
2023-03-07 22:13:42 +0900

    조인때문에 여러개 데이터가 나옴. 테이블입장에선 당연

--- JPA_02_PRACTICE/src/main/java/book/shop/repository/OrderRepository.java
+++ JPA_02_PRACTICE/src/main/java/book/shop/repository/OrderRepository.java
@@ -81,4 +81,14 @@ public class OrderRepository {
                                 " join o.delivery d", OrderSimpleQueryDto.class)
                 .getResultList();
     }
+
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d" +
+                        " join fetch o.orderItems oi" +
+                        " join fetch oi.item i", Order.class)
+                .getResultList();
+    }
 }
