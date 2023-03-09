6e5e881a5b2d756de6e0d836e1cfcba5ce07a8e7
Woosung_Kim <sibu2005@naver.com>
2023-03-09 22:43:34 +0900

    flat하게 가져와서 application에서 map 돌려서 맞는 dto로 변환

--- JPA_02_PRACTICE/src/main/java/book/shop/api/OrderApiController.java
+++ JPA_02_PRACTICE/src/main/java/book/shop/api/OrderApiController.java
@@ -2,6 +2,8 @@ package book.shop.api;
 
 import book.shop.domain.*;
 import book.shop.repository.OrderRepository;
+import book.shop.repository.order.query.OrderFlatDto;
+import book.shop.repository.order.query.OrderItemQueryDto;
 import book.shop.repository.order.query.OrderQueryDto;
 import book.shop.repository.order.query.OrderQueryRepository;
 import lombok.Data;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -40,16 +44,17 @@ public class OrderApiController {
         List<Order> orders = orderRepository.findAllByString(new OrderSearch());
         List<OrderDto> collect = orders.stream()
                 .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
+                .collect(toList());
 
         return collect;
     }
+
     @GetMapping("/api/v3/orders")
     public List<OrderDto> ordersV3() {
         List<Order> orders = orderRepository.findAllWithItem();
         List<OrderDto> collect = orders.stream()
                 .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
+                .collect(toList());
 
         return collect;
     }
@@ -62,10 +67,11 @@ public class OrderApiController {
         List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
         List<OrderDto> collect = orders.stream()
                 .map(o -> new OrderDto(o))
-                .collect(Collectors.toList());
+                .collect(toList());
 
         return collect;
     }
+
     @GetMapping("/api/v4/orders")
     public List<OrderQueryDto> ordersV4() {
         return orderQueryRepository.findOrderQueryDtos();
@@ -74,6 +80,14 @@ public class OrderApiController {
     @GetMapping("/api/v5/orders")
     public List<OrderQueryDto> ordersV5() {
         return orderQueryRepository.findAllByDto_optimization();
+    }
+
+    @GetMapping("/api/v6/orders")
+    public List<OrderQueryDto> ordersV6() {
+        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+        return flats.stream()
+                .collect(groupingBy(o -> new OrderQueryDto(o.getOrderId(), o.getName(), o.getOrderDate(), o.getOrderStatus(), o.getAddress()),
+                        mapping(o -> new OrderItemQueryDto(o.getOrderId(), o.getItemName(), o.getOrderPrice(), o.getCount()), toList()))).entrySet().stream().map(e -> new OrderQueryDto(e.getKey().getOrderId(), e.getKey().getName(), e.getKey().getOrderDate(), e.getKey().getOrderStatus(), e.getKey().getAddress(), e.getValue())).collect(toList());
     }
 
 
@@ -94,8 +108,8 @@ public class OrderApiController {
             this.orderStatus = order.getStatus();
             this.address = order.getDelivery().getAddress();
             this.orderItems = order.getOrderItems().stream()
-                    .map(orderItem ->  new OrderItemDto(orderItem))
-                    .collect(Collectors.toList());
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(toList());
         }
     }
 
--- JPA_02_PRACTICE/src/main/java/book/shop/repository/order/query/OrderQueryDto.java
+++ JPA_02_PRACTICE/src/main/java/book/shop/repository/order/query/OrderQueryDto.java
@@ -3,11 +3,14 @@ package book.shop.repository.order.query;
 import book.shop.domain.Address;
 import book.shop.domain.OrderStatus;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collector;
 
 @Data
+@EqualsAndHashCode(of = "orderId")
 public class OrderQueryDto {
     private Long orderId;
     private String name;
@@ -23,4 +26,13 @@ public class OrderQueryDto {
         this.orderStatus = orderStatus;
         this.address = address;
     }
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> orderItems) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = orderItems;
+    }
+
 }
