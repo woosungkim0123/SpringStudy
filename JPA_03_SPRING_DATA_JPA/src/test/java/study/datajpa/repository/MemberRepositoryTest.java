93d610d38b21c99cb03fba87de4be2a691fd41b1
Woosung_Kim <sibu2005@naver.com>
2023-03-10 23:10:59 +0900

    Spring data jpa 테스트

---
+++ JPA_03_SPRING_DATA_JPA/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,30 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("우성");
+        Member savedMember = memberRepository.save(member);
+        Optional<Member> byId = memberRepository.findById(member.getId());
+        Member findMember = byId.get();
+
+        assertThat(savedMember.getId()).isEqualTo(findMember.getId());
+        assertThat(savedMember.getUsername()).isEqualTo(findMember.getUsername());
+    }
+}
