b68841b1b28621209072059c917efe3fb38b0d7b
Woosung_Kim <sibu2005@naver.com>
2023-03-10 23:15:24 +0900

    Spring data jpa 테스트 rollback 추가

--- JPA_03_SPRING_DATA_JPA/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ JPA_03_SPRING_DATA_JPA/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -3,6 +3,7 @@ package study.datajpa.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
@@ -13,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+@Rollback(false)
 class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
