e1803176e5169040f340e0cbf203181e496dc27b
Woosung_Kim <sibu2005@naver.com>
2023-03-12 00:36:37 +0900

    jpa만을 활용하여 repository 만들기

--- JPA_03_SPRING_DATA_JPA/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ JPA_03_SPRING_DATA_JPA/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -6,6 +6,8 @@ import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
 import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,6 +20,26 @@ public class MemberJpaRepository {
         em.persist(member);
         return member;
     }
+
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
+    }
+
 
     public Member find(Long id) {
         return em.find(Member.class, id);
