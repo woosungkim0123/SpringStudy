b70a9def5e92aaa0f9dc864121682dfaa5641aa8
Woosung_Kim <sibu2005@naver.com>
2023-03-12 00:02:26 +0900

    lazy 세팅

--- JPA_03_SPRING_DATA_JPA/src/main/java/study/datajpa/entity/Member.java
+++ JPA_03_SPRING_DATA_JPA/src/main/java/study/datajpa/entity/Member.java
@@ -18,7 +18,7 @@ public class Member {
     private String username;
     private int age;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "team_id")
     private Team team;
 
