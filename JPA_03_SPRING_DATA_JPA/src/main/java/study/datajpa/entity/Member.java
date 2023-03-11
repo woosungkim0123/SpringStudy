ef917a5b8ccc03aa08b53727db8afb20fc833cab
Woosung_Kim <sibu2005@naver.com>
2023-03-11 23:58:48 +0900

    엔티티 제대로 동작하는지 테스트

--- JPA_03_SPRING_DATA_JPA/src/main/java/study/datajpa/entity/Member.java
+++ JPA_03_SPRING_DATA_JPA/src/main/java/study/datajpa/entity/Member.java
@@ -26,6 +26,15 @@ public class Member {
         this.username = username;
     }
 
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null) {
+            changeTeam(team);
+        }
+
+    }
+
     public void changeTeam(Team team) {
         this.team = team;
         team.getMembers().add(this);
