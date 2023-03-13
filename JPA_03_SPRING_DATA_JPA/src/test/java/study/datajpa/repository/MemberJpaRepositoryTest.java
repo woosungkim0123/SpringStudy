70bbcaf55f760a064974ff6b4f8dcb3b672cc168
Woosung_Kim <sibu2005@naver.com>
2023-03-13 14:05:07 +0900

    member에 대한 테스트 케이스 작성. 이후 다른 변경이 일어나도 테스트 케이스를 통해 안정감 획득

--- JPA_03_SPRING_DATA_JPA/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ JPA_03_SPRING_DATA_JPA/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -4,16 +4,20 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class MemberJpaRepositoryTest {
 
     @Autowired MemberJpaRepository memberJpaRepository;
-
 
     @Test
     public void testMember() {
@@ -25,4 +29,34 @@ class MemberJpaRepositoryTest {
         assertThat(savedMember.getId()).isEqualTo(findMember.getId());
         assertThat(savedMember.getUsername()).isEqualTo(findMember.getUsername());
     }
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        findMember1.changeName("member10");
+        assertThat(findMember1.getUsername()).isEqualTo("member10");
+
+
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        memberJpaRepository.delete(member1);
+
+        long deleteCount = memberJpaRepository.count();
+        assertThat(deleteCount).isEqualTo(1);
+
+    }
+
 }
