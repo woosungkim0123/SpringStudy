54e55ad309d70562c36bf51effba7cac7d63d98f
Woosung_Kim <sibu2005@naver.com>
2023-03-23 19:07:35 +0900

    querydsl

--- JPA_04_QUERYDSL/src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ JPA_04_QUERYDSL/src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -2,12 +2,14 @@ package study.querydsl.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 
 import java.util.List;
 import java.util.Optional;
+
+import static study.querydsl.entity.QMember.*;
 
 @Repository
 public class MemberJpaRepository {
@@ -35,9 +37,21 @@ public class MemberJpaRepository {
                 .getResultList();
     }
 
+    public List<Member> findAll_Querydsl() {
+        return queryFactory.selectFrom(member).fetch();
+    }
+
     public List<Member> findByUsername(String username) {
         return em.createQuery("select m from Member m where m.username = :username", Member.class)
                 .setParameter("username", username)
                 .getResultList();
     }
+
+    public List<Member> findByUsername_Querydsl(String username) {
+        return queryFactory
+                .selectFrom(member)
+                .where(member.username.eq(username))
+                .fetch();
+    }
+
 }
