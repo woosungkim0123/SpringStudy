05684d6a5a1a4f2e5ce179ba94e115adfbaf69be
Woosung_Kim <sibu2005@naver.com>
2023-03-23 22:28:40 +0900

    querydsl test

--- JPA_04_QUERYDSL/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ JPA_04_QUERYDSL/src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -17,8 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @Transactional
 class MemberJpaRepositoryTest {
 
-    @Autowired
-    EntityManager em;
+    @Autowired EntityManager em;
     @Autowired MemberJpaRepository memberJpaRepository;
 
     @Test
@@ -34,7 +33,21 @@ class MemberJpaRepositoryTest {
 
         List<Member> result2 = memberJpaRepository.findByUsername("member1");
         assertThat(result2).containsExactly(member);
-
-
     }
+
+    @Test
+    public void querydslTest() {
+        Member member = new Member("member1", 10);
+        memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.findById(member.getId()).get();
+        assertThat(findMember).isEqualTo(member);
+
+        List<Member> result1 = memberJpaRepository.findAll_Querydsl();
+        assertThat(result1).containsExactly(member);
+
+        List<Member> result2 = memberJpaRepository.findByUsername_Querydsl("member1");
+        assertThat(result2).containsExactly(member);
+    }
+
 }
